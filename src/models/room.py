44c598370dbfda8a2cc0202dc8a416752e76374f
Ahmed Shanshal <ahmedmahdii2003@gmail.com>
2026-04-24 15:47:08 +0300

    feat: room model

--- src/models/room.py
+++ src/models/room.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -7,17 +9,10 @@ from src.db.session import Base
 class Room(Base):
     __tablename__ = "rooms"
 
-    # Table: rooms
-    # Properties:
-    # - id: primary key
-    # - name: room name/label
-    # - code: optional room code
-    # - deleted_at: optional soft delete timestamp
-
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), index=True)
+    deleted_at: Mapped[datetime | None] = mapped_column(default=None)
 
     # TODO: Instruments reference rooms as their current location.
-    # TODO: No room sub-location model is planned.
     # TODO: A room can be linked to many devices.
     # TODO: Movement requirements are enforced through transition rules and event logs.
